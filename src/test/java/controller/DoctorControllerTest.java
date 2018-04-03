b9dfcab183428f11ce696b334598aa0b52d58612
Butiri Cristian <butiri.cristian@gmail.com>
2018-04-04 00:06:50 +0300

    finalizing tests

--- src/test/java/controller/DoctorControllerTest.java
+++ src/test/java/controller/DoctorControllerTest.java
@@ -27,6 +27,96 @@ public class DoctorControllerTest {
         catch (PatientException e){
             assert (true);
         }
+    }
 
+    @Test
+    public void addLongCNP() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("John Doe", "1234567890123456", "addr1"));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addShortCNP() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("John Doe", "12345", "addr1"));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addSmallLettersCNP() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("John Doe", "abc", "addr1"));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addCapitalLettersCNP() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("John Doe", "ABC", "addr1"));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addZeroLengthName() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("", "1234567890123", "addr1"));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addNameWithDigits() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("abc123", "1234567890123", "addr1"));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addZeroLengthAddress() throws Exception {
+        Repository repository = new Repository("src/main/FilePatients.txt", "src/main/FileConsultations.txt");
+        DoctorController dc = new DoctorController(repository);
+        try {
+            dc.addPatient(new Patient("John Doe", "1234567890123", ""));
+            assert (false);
+        }
+        catch (PatientException e){
+            assert (true);
+        }
     }
 }
